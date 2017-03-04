6615d1d1e4a13e6d7b3edeb7b30179755577012e
Axiom <plwest.axiom@gmail.com>
2017-03-04 17:53:01 -0500

    Initial Commit with TCP client and service

--- DesignPatterns/Patterns/Properties/AssemblyInfo.cs
+++ DesignPatterns/Patterns/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Factory")]
+[assembly: AssemblyTitle("Patterns")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Factory")]
+[assembly: AssemblyProduct("Patterns")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("e6c2c271-3d59-471c-a5cb-a80560ba050f")]
+[assembly: Guid("f1c53ccc-8612-4320-b820-b5b27f0f3965")]
 
 // Version information for an assembly consists of the following four values:
 //
